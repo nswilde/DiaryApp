45336b42bb23232c10cadbf55258fa25388b6829
[Nswilde] <142635621+nswilde@users.noreply.github.com>
2024-02-07 01:16:04 +1300

    Adding award icons and JSON Decoder extension

---
+++ Diary/Award.swift
@@ -0,0 +1,21 @@
+//
+//  Award.swift
+//  Diary
+//
+//  Created by Nikki Wilde on 7/02/24.
+//
+
+import Foundation
+
+struct Award: Decodable, Identifiable {
+    var id: String { name }
+    var name: String
+    var description: String
+    var color: String
+    var criterion: String
+    var value: Int
+    var image: String
+    
+    static let allAwards = Bundle.main.decode("Awards.json", as: [Award].self)
+    static let example = allAwards[0]
+}
---
+++ Diary/AwardsView.swift
@@ -0,0 +1,36 @@
+//
+//  AwardsView.swift
+//  Diary
+//
+//  Created by Nikki Wilde on 7/02/24.
+//
+
+import SwiftUI
+
+struct AwardsView: View {
+    var columns: [GridItem] {
+        [GridItem(.adaptive(minimum: 100, maximum: 100))]
+    }
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(Award.allAwards) { award in
+                        Button {
+                            //button
+                        } label: {
+                            Image(systemName: award.image)
+                                .resizable()
+                                .scaledToFit()
+                                .padding()
+                                .frame(width: 100, height: 100)
+                                .foregroundColor(.secondary.opacity(0.5))
+                        }
+                    }
+                }
+                .navigationTitle("Awards")
+            }
+        }
+    }
+}
