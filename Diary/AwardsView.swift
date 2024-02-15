5ed99ebbb49a133bb198345b3f530eb7d5defdf9
[Nswilde] <142635621+nswilde@users.noreply.github.com>
2024-02-16 12:04:14 +1300

    Started adding screen reader, tidying app

--- Diary/AwardsView.swift
+++ Diary/AwardsView.swift
@@ -42,6 +42,8 @@ struct AwardsView: View {
                                 .foregroundColor(dataController.hasEarned(award: award) ? Color(award.color) :
                                         .secondary.opacity(0.5))
                         }
+                        .accessibilityLabel(dataController.hasEarned(award: award) ? "Unlocked: \(award.name)" : "Locked")
+                        .accessibilityHint(award.description)
                     }
                 }
             }
